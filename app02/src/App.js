77725d523505513793e39ac9c9763eeb4364c845
YoonArum <ckdalsag@naver.com>
2022-10-05 17:13:16 +0900

    리액트 시계

---
+++ app02/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import MyTick from './MyCom/MyTick';
+
+function App() {
+  console.log("App")
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />        
+      </header>
+      <MyTick />
+            
+      
+    </div>
+  );
+}
+
+export default App;
